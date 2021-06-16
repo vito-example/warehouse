3187bd2afc7a49e5b6f8a6e60a7eb4cdf7f45c68
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 21:17:06 +0400

    Add supplier model softdeletes, fillable and table field.

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Supplier
@@ -16,5 +17,21 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Supplier extends Model
 {
-    use HasFactory;
+    use HasFactory,softDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'suppliers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
 }
