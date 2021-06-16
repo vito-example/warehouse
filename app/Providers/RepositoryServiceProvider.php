a0023d9089f45fa003f0f13f24a3fc8b4364a12a
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 21:06:29 +0400

    Make RepositoryServiceProvider and register into config providers

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * app\Providers\RepositoryServiceProvider.php
+ *
+ * Date-Time: 6/16/2021
+ * Time: 9:03 PM
+ *
+ * @author Vito Makhatadze <vitomakhatadze@gmail.com>
+ */
+
+namespace App\Providers;
+
+use App\Repositories\Eloquent\Base\BaseRepository;
+use App\Repositories\Eloquent\Base\EloquentRepositoryInterface;
+use App\Repositories\Eloquent\ProductRepository;
+use App\Repositories\Eloquent\SupplierRepository;
+use App\Repositories\Eloquent\TransferRepository;
+use App\Repositories\Eloquent\WareHouseRepository;
+use App\Repositories\ProductRepositoryInterface;
+use App\Repositories\SupplierRepositoryInterface;
+use App\Repositories\TransferRepositoryInterface;
+use App\Repositories\WareHouseRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class RepositoryServiceProvider
+ * @package App\Providers
+ */
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+
+    }
+}
