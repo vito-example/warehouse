5c731486810536fc1d21aaa25e0ff053538eb827
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 22:11:34 +0400

    Add warehouse apiResource to routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\SupplierController;
+use App\Http\Controllers\Api\V1\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,9 @@ Route::prefix('v1')->group(function () {
 
     // Supplier Module
     Route::apiResource('supplier',SupplierController::class);
+
+    // Warehouse Module
+    Route::apiResource('warehouse',WarehouseController::class);
 });
 
 
