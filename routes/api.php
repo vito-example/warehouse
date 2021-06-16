798e3af54f970a918f9d78c075eb0c3757b39507
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 21:20:51 +0400

    Add supplier module apiResource to routes.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\SupplierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    // Supplier Module
+    Route::apiResource('supplier',SupplierController::class);
 });
+
+
