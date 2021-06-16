e854dfed429a7e1d0b36ebd27343cbac08d1caf5
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 21:26:11 +0400

    Add SupplierRequest for validation.

---
+++ app/Http/Requests/Api/V1/SupplierRequest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * app\Http\Requests\Api\V1\SupplierRequest.php
+ *
+ * Date-Time: 6/16/2021
+ * Time: 9:25 PM
+ *
+ * @author Vito Makhatadze <vitomakhatadze@gmail.com>
+ */
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class SupplierRequest
+ * @package App\Http\Requests\Api\V1
+ */
+class SupplierRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        // Check if method is get,fields are nullable.
+        $isRequired = $this->method() === 'GET' ? 'nullable' : 'required';
+
+        return [
+            'name' => [$isRequired, 'string', 'max:255', Rule::unique('suppliers', 'name')->ignore($this->supplier)],
+        ];
+    }
+}
