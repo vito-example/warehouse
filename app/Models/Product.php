c72d0c74b5deec1cda6291002792203400c79ec2
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 22:19:23 +0400

    Make product model with migration and scopeFIlter.

---
+++ app/Models/Product.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * app\Models\Product.php
+ *
+ * Date-Time: 6/16/2021
+ * Time: 10:18 PM
+ *
+ * @author Vito Makhatadze <vitomakhatadze@gmail.com>
+ */
+namespace App\Models;
+
+use App\Traits\ScopeFilter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Product
+ * @package App\Models
+ * @property integer $id
+ * @property integer $supplier_id
+ * @property string $name
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string|null $deleted_at
+ */
+class Product extends Model
+{
+    use HasFactory, softDeletes, ScopeFilter;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'supplier_id',
+        'name'
+    ];
+
+    /**
+     * @var string
+     */
+    public $defaultSort = 'created_at';
+    /**
+     * @var string
+     */
+    public $defaultOrder = 'desc';
+
+    /**
+     * @return array[]
+     */
+    public function getFilterScopes(): array
+    {
+        return [
+            'id' => [
+                'hasParam' => true,
+                'scopeMethod' => 'id'
+            ],
+            'name' => [
+                'hasParam' => true,
+                'scopeMethod' => 'name'
+            ],
+        ];
+    }
+}
---
+++ database/migrations/2021_06_16_181736_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * database\migrations\2021_06_16_181736_create_products_table.php
+ *
+ * Date-Time: 6/16/2021
+ * Time: 10:17 PM
+ *
+ * @author Vito Makhatadze <vitomakhatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplier_id')->constrained('suppliers');
+            $table->index(['supplier_id']);
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
