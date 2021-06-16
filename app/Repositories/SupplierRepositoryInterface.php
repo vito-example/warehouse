313c41ac433126bd551504aa2f454c5cbe3e5853
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 21:35:35 +0400

    Make getData interface method in SupplierRepositoryInterface.

--- app/Repositories/SupplierRepositoryInterface.php
+++ app/Repositories/SupplierRepositoryInterface.php
@@ -10,11 +10,18 @@
 
 namespace App\Repositories;
 
+use App\Http\Requests\Api\v1\SupplierRequest;
+
 /**
  * Interface SupplierRepositoryInterface
  * @package App\Repositories
  */
 interface SupplierRepositoryInterface
 {
-
+    /**
+     * @param SupplierRequest $request
+     * @param array $with
+     * @return mixed
+     */
+    public function getData(SupplierRequest $request, array $with = []);
 }
