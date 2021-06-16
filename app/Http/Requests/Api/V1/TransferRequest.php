ecf5383269db73d128ded9aa2dd2926e23d31e2b
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-16 22:56:32 +0400

    Make transfer request.

---
+++ app/Http/Requests/Api/V1/TransferRequest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * app\Http\Requests\Api\V1\TransferRequest.php
+ *
+ * Date-Time: 6/16/2021
+ * Time: 10:55 PM
+ *
+ * @author Vito Makhatadze <vitomakhatadze@gmail.com>
+ */
+namespace App\Http\Requests\Api\V1;
+
+/**
+ * Class TransferRequest
+ * @package App\Http\Requests\Api\v1
+ */
+class TransferRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
