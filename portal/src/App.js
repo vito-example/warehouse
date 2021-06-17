cb27eedd4f7170a9a62497ee952e238a5a0b6f4e
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-17 19:42:07 +0400

    Create fully layout for our project.

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
 import {Provider} from "react-redux";
 import store from "./store";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
 import 'react-toastify/dist/ReactToastify.css'
 import 'antd/dist/antd.css'
 import 'antd/dist/antd.js'
 import {ToastContainer} from "react-toastify";
 import './App.scss';
+import NavLeft from "./components/NavLeft/NavLeft";
+import CHeader from "./components/Header/Header";
+import Supplier from "./modules/Supplier/Supplier";
+import Layout, {Content, Footer} from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 
 const App =() => (
     <Provider store={store}>
-
+        <Router>
+            <Route exact path='/'>
+                <Redirect to='/supplier'/>
+            </Route>
+            <Layout className="layout-wrapper">
+                <Sider className="nav" trigger={null} collapsible collapsed={false}>
+                    <NavLeft/>
+                </Sider>
+                <Layout className="main-wrapper">
+                    <CHeader/>
+                    <Content className="content">
+                        <ToastContainer />
+                        <Switch>
+                            <Route exact path="/supplier" component={Supplier}/>
+                        </Switch>
+                    </Content>
+                    <Footer className="footer">
+                        <p>Copyright © 2021</p>
+                    </Footer>
+                </Layout>
+            </Layout>
+        </Router>
     </Provider>
 )
 
---
+++ portal/src/modules/Supplier/Supplier.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Supplier extends Component{
+    render() {
+        return <h1>Supplier</h1>
+    }
+}
+
+
+export default Supplier
