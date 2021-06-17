8d990adda82b34add25753f60438f91b1ee67b48
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-17 19:34:22 +0400

    Add CHeader component for site header.

---
+++ portal/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Layout} from 'antd';
+import "./Header.scss";
+
+const {Header} = Layout;
+
+const CHeader = () => (
+    <Header className="header">
+        header
+    </Header>
+)
+
+export default CHeader;
