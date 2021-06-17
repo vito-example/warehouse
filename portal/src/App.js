b5accadf7bea27b2c947a32fde0d362bdb384950
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-17 19:31:29 +0400

    Register assets.

--- portal/src/App.js
+++ portal/src/App.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import {Provider} from "react-redux";
 import store from "./store";
 
+import 'react-toastify/dist/ReactToastify.css'
+import 'antd/dist/antd.css'
+import 'antd/dist/antd.js'
+import {ToastContainer} from "react-toastify";
+import './App.scss';
 
 const App =() => (
     <Provider store={store}>
