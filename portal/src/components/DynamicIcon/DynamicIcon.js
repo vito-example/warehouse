88e7e0b850b26d406a86afd6e5ba135fad9db896
makhatadze <45604845+makhatadze@users.noreply.github.com>
2021-06-17 19:33:10 +0400

    Make DynamicIcon loadable component for antd design icons.

---
+++ portal/src/components/DynamicIcon/DynamicIcon.js
@@ -0,0 +1,9 @@
+import loadable from "@loadable/component";
+
+const DynamicIcon = loadable(props =>
+    import(`@ant-design/icons/es/icons/${props.type}.js`)
+        .catch(err => import(`@ant-design/icons/es/icons/WarningOutlined.js`)), {
+    cacheKey: props => props.type
+})
+
+export default DynamicIcon;
